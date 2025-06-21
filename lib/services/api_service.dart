af69f2f752705ccc4986fc564c18049337c90b40
Yash Chandra <yash24.chandra2003@gmail.com>
2025-06-22 00:25:21 +0530

    Readme Updated

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,14 +4,14 @@ import '../models/product_model.dart';
 
 class ApiService {
   static const String baseUrl =
-      'http://192.168.29.253:3000'; // Use your local IP
+      'http://<Your-IP-Address>:3000'; // Use your local IP
 
   static Future<List<Product>> fetchProducts() async {
     final response = await http.get(Uri.parse('$baseUrl/products'));
 
     if (response.statusCode == 200) {
       final List decoded = jsonDecode(response.body);
-      return decoded.map((e) => Product.fromJson(e)).toList();
+      return decoded.map((e) => Product.fromJson(e)).toList();  
     } else {
       throw Exception('Failed to load products');
     }
